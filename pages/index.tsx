4b73a916cafe7ec9638e2e11ad7fc59956d51e24
Eduardo Cobuci <educobuci@gmail.com>
2021-08-30 20:58:33 -0300

    Config TS and Tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <div className="p-4">
+    <h1 className="text-3xl">Auction Snipper</h1>
+  </div>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  // mode: 'jit',
+  purge: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      objectPosition: {
+        center88: '88% center'
+      },
+      boxShadow: {
+        card: '0px 3px 100px #00000020'
+      },
+      borderRadius: {
+        '4xl': '2rem'
+      },
+      colors: {
+        primary: colors.blue,
+        gray: colors.coolGray,
+        gridTemplateColumns: {
+          'fill': 'repeat(auto-fill, minmax(0, 1fr))',
+        },
+      },
+      fontSize: {
+        'huge': '13rem'
+      },
+      lineHeight: {
+        'full': '100%'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')]
+}
