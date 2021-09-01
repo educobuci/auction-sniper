f6f911c46c62277bf025f77cb26fb1f8b54ae24c
Eduardo Cobuci <educobuci@gmail.com>
2021-09-01 16:28:46 -0300

    Fix typo

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ const config =  {
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
   cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
   appId: process.env.PUSHER_APP_ID,
-  secret: process.env.PUSHER_APP_SECRETE,
+  secret: process.env.PUSHER_APP_SECRET,
 }
 
 export { config }
