e411190e94c088acc61afe1807d43dacdafd7dbe
Eduardo Cobuci <educobuci@gmail.com>
2021-09-02 12:37:05 -0300

    Add redundant env var for vercel

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 const config =  {
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY || process.env.PUSHER_APP_KEY,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER || process.env.PUSHER_APP_CLUSTER,
   appId: process.env.PUSHER_APP_ID,
   secret: process.env.PUSHER_APP_SECRET,
 }
